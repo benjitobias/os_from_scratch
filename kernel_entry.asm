252c88771480cd76c7de7a84ed50cfe2c2917c62
Benji <benjitobias@gmail.com>
2020-08-08 18:22:18 +0300

    Enter kernel

---
+++ kernel_entry.asm
@@ -0,0 +1,4 @@
+[bits32]
+[extern main] ; Define calling point. Must have the same name as kernel.c 'main' function
+call main ; Calls the C funtion. The linker will know where it is places in memory
+jmp $
