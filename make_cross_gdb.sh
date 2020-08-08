95f8d531ce4c42841924dc702af6eb7a79bc6d7f
Benji <benjitobias@gmail.com>
2020-08-08 22:58:03 +0300

    make cross-compiled gdb

---
+++ make_cross_gdb.sh
@@ -0,0 +1,10 @@
+cd /tmp/src
+curl -O http://ftp.rediris.es/mirror/GNU/gdb/gdb-9.2.tar.xz
+tar xf gdb-9.2.tar.xz
+mkdir gdb-build
+cd gdb-build
+export PREFIX="/usr/local/i386elfgcc"
+export TARGET=i386-elf
+../gdb-9.2/configure --target="$TARGET" --prefix="$PREFIX" --program-prefix=i386-elf-
+make
+make install
